5a30bdd6c10c50e2bde7247692fc1d74484c6e3a
Chad Sikorra <Chad.Sikorra@gmail.com>
2021-01-03 11:31:27 -0600

    Missing license header.

--- src/K8s/ApiGenerator/Command/ApiVersionsCommand.php
+++ src/K8s/ApiGenerator/Command/ApiVersionsCommand.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * This file is part of the k8s/api-generator library.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 declare(strict_types=1);
 
 namespace K8s\ApiGenerator\Command;
